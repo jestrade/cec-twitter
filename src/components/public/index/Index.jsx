d1dae8ebf3aac1320a8def59042122e34059c10f
Jesus Estrada <jesuestrada@gmail.com>
2022-06-28 21:13:17 -0500

    home ui

--- src/components/public/index/Index.jsx
+++ src/components/public/index/Index.jsx
@@ -1,19 +1,25 @@
 import { Link } from "react-router-dom";
 
 import Logo from "./../../common/logo/Logo";
+import "./style.css";
+import "./../../main/style.css";
 
 const Index = () => {
 
-    return <main>
-        <div></div>
-        <div>
-            <p><Logo /></p>
-            <h1>Welcome to Twitter</h1>
-            <p><Link to="/login">Login now</Link></p>
-            <p>Dont have an account?
-                <Link to="/login">Join free today</Link>
-                <Link to="signup.html">Sign
-                    Up</Link></p>
+    return <main className="home">
+        <div className="column leftColumn">
+            <Logo size="big" />
+        </div>
+        <div className="column rightColumn">
+            <div>
+                <p><Logo /></p>
+                <h1>Welcome to Twitter</h1>
+                <p className="paragraphLarge"><Link to="/login" className="button buttonSecondary">Login now</Link></p>
+                <p className="paragraphSmall">Dont have an account?
+                    <Link to="/login">Join free today</Link>
+                    <Link to="/signup" className="button buttonSecondary">Sign
+                        Up</Link></p>
+            </div>
         </div>
     </main>
 };
